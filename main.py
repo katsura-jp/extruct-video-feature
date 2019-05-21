985434d13689e26741180e0600270d286ac24e44
katsura_jp <nok.addayo@gmail.com>
2019-05-21 16:19:31 +0900

    change output dimention

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import models
 c3d = models.c3d
 
 from loader import VideoLoader
-from utils import convert_param
+from utils import convert_param,activity_name,pool_feature
 from options import Opt
 
 
@@ -32,11 +32,13 @@ def main():
     out_dir = args.out_dir
     visual_feature_out_dir = os.path.join(out_dir, 'visual-feature')
     activity_concept_out_dir = os.path.join(out_dir, 'visual-activity-concept')
+    pooled_activity_concept_out_dir = os.path.join(out_dir, 'activity-concept')
 
     print('output directory : {}'.format(out_dir))
     os.makedirs(out_dir, exist_ok=True)
     os.makedirs(visual_feature_out_dir, exist_ok=True)
     os.makedirs(activity_concept_out_dir, exist_ok=True)
+    os.makedirs(pooled_activity_concept_out_dir, exist_ok=True)
 
     root_dir = glob.glob(os.path.join(args.root_dir,'*'))
 
@@ -64,12 +66,22 @@ def main():
             # torch.cuda -> np.array
             visual_feature = visual_feature.cpu().detach().numpy()
             visual_activity_concept = visual_activity_concept.cpu().detach().numpy()
+            # convert matrix to vector : (1,outdim) --> (outdim,)
+            visual_feature = visual_feature.reshape(visual_feature.shape[1],)
+            visual_activity_concept = visual_activity_concept.reshape(visual_activity_concept.shape[1],)
+            # save feature
             np.save(os.path.join(visual_feature_out_dir, name), visual_feature)
-            np.save(os.path.join(activity_concept_out_dir, name), visual_activity_concept)
+            np.save(os.path.join(activity_concept_out_dir, activity_name(name)), visual_activity_concept)
             if args.verbose:
                 print('save {}.npy'.format(name))
 
+    # get movie name
+    movie_names = []
+    for movie in root_dir:
+        movie_names.append(movie.split('/')[-1])
 
+    for movie_name in progress_bar(movie_names):
+        pool_feature(movie_name, activity_concept_out_dir, pooled_activity_concept_out_dir)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
-
+import numpy as np
+import os
 
 def convert_param(param7, param_py, verbose=False):
     params = param_py.copy()
@@ -34,3 +35,33 @@ def convert_param(param7, param_py, verbose=False):
                 idx += 1
 
     return params
+
+
+def activity_name(name):
+    movie_name, start, end = name.split('_')
+    new_name = movie_name + '.mp4_' + start.split('.')[0] + "_" + end.split('.')[0]
+    return new_name
+
+def pool(x, y):
+    return (x + y) / 2.0
+
+
+def get_file_name(movie_name, s, e):
+    return f'{movie_name}_{s}_{e}.npy'
+
+def pool_feature(movie_name, activity_dir, out_dir):
+    s = 1
+    step = 16
+    nxt = 8
+    while(1):
+        path1 = activity_dir + get_file_name(movie_name, s, s+step) # 1-17
+        path2 = activity_dir + get_file_name(movie_name, s+step, s+step+step) # 17-33
+        if ((not os.path.exists(path1)) or (not os.path.exists(path2))):
+            break
+        else:
+            f1 = np.load(path1)
+            f2 = np.load(path2)
+            pooled = pool(f1, f2)
+            name = get_file_name(movie_name, s, s+step+step) # 1-33
+            np.save(out_dir + name, pooled)
+        s += nxt
